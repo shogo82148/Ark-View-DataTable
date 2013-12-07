840763cb9c9f93177c146b73c4f8fa97aaf3260f
Ichinose Shogo <shogo82148@gmail.com>
2013-12-07 14:48:25 +0900

    support json

--- lib/Ark/View/DataTable.pm
+++ lib/Ark/View/DataTable.pm
@@ -3,8 +3,24 @@ use 5.008005;
 use strict;
 use warnings;
 use Ark 'View';
-
+use JSON;
 our $VERSION = "0.01";
+
+has json_driver => (
+    is      => 'rw',
+    isa     => 'Object',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        JSON->new->utf8->allow_nonref;
+    },
+);
+
+has 'responseHandler' => (
+    is => 'rw',
+    isa => 'Str',
+    default => 'google.visualization.Query.setResponse'
+);
 
 sub process {
     my ($self, $c) = @_;
@@ -26,7 +42,40 @@ sub process {
 
 sub _process_json {
     my ($self, $c, $tqx) = @_;
+    my $_placeholder = 'hfaeghoucyrhksfhladsoijdsflkahu2u9';
+    my $payload = $self->_render_json($c, $tqx);
+    my $table;
+
+    if($payload->{table}) {
+        $table = $payload->{table};
+        $payload->{table} = $_placeholder;
+    }
+    my $data = $self->json_driver->encode($payload);
+    if($table) {
+        $data =~ s/"$_placeholder"/$table/;
+    }
+
+    my $responseHandler = $tqx->{responseHandler} || $self->responseHandler;
+    $data = sprintf('%s(%s);', $responseHandler, $data);
+    $c->response->body( $data );
+}
+
+sub _render_json {
+    my ($self, $c, $tqx) = @_;
     my $payload = {};
+    $payload->{reqId} = $tqx->{reqId} if exists $tqx->{reqId};
+    if($c->stash->{errors}) {
+        $payload->{status} = 'error';
+        $payload->{errors} = $c->stash->{errors};
+        return $payload;
+    } elsif($c->stash->{warnings}) {
+        $payload->{status} = 'warning';
+        $payload->{warnings} = $c->stash->{warnings};
+    } else {
+        $payload->{status} = 'ok';
+    }
+    $payload->{table} = $c->stash->{table}->output_javascript;
+    return $payload;
 }
 
 sub _process_html {
