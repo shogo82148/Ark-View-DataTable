d89a0949025a4d7eb2f42cffe13bde0492fab87f
Ichinose Shogo <shogo82148@gmail.com>
2013-12-07 14:20:22 +0900

    parse tqx

--- lib/Ark/View/DataTable.pm
+++ lib/Ark/View/DataTable.pm
@@ -2,12 +2,47 @@ package Ark::View::DataTable;
 use 5.008005;
 use strict;
 use warnings;
+use Ark 'View';
 
 our $VERSION = "0.01";
 
+sub process {
+    my ($self, $c) = @_;
+    my $tqx = { map { split /:/, $_, 2} split /;/, $c->request->param('tqx') };
+    my $out = $tqx->{out} || 'json';
 
+    if($out eq 'json') {
+        $self->_process_json($c, $tqx);
+    } elsif($out eq 'html') {
+        $self->_process_html($c, $tqx);
+    } elsif($out eq 'csv') {
+        $self->_process_csv($c, $tqx);
+    } elsif($out eq 'tsv-excel') {
+        $self->_process_tsv_excel($c, $tqx);
+    } else {
+        die 'unsupported out type: ' . $out;
+    }
+}
 
-1;
+sub _process_json {
+    my ($self, $c, $tqx) = @_;
+    my $payload = {};
+}
+
+sub _process_html {
+    my ($self, $c, $tqx) = @_;
+}
+
+sub _process_csv {
+    my ($self, $c, $tqx) = @_;
+}
+
+sub _process_tsv_excel {
+    my ($self, $c, $tqx) = @_;
+}
+
+__PACKAGE__->meta->make_immutable;;
+
 __END__
 
 =encoding utf-8
