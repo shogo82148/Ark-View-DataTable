ce80dabb70ec119e6b01c419b69bb2be6dbb7207
Ichinose Shogo <shogo82148@gmail.com>
2013-12-07 13:10:15 +0900

    init commit

---
+++ lib/Ark/View/DataTable.pm
@@ -0,0 +1,39 @@
+package Ark::View::DataTable;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Ark::View::DataTable - It's new $module
+
+=head1 SYNOPSIS
+
+    use Ark::View::DataTable;
+
+=head1 DESCRIPTION
+
+Ark::View::DataTable is ...
+
+=head1 LICENSE
+
+Copyright (C) Ichinose Shogo.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Ichinose Shogo E<lt>shogo82148@gmail.comE<gt>
+
+=cut
+
